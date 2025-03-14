9f474d11ead14555fe51d45e774d19271abfd333
aladinsws <aladinsws@gmail.com>
2025-03-14 01:02:09 +0100

    create Space Cat NFT

--- src/Assignment8.sol
+++ src/Assignment8.sol
@@ -2,30 +2,28 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";    
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 // make this contract inherits from: ERC721URIStorage contract
-contract Assignment8 {
+contract Assignment8 is ERC721URIStorage, Ownable {
     // declare a private uint called _tokenIdCounter
+    uint private _tokenIdCounter;
 
     // pass name and symbol of the nft token collection
-    constructor() ERC721() {
-        
-    }
+    constructor() ERC721("Space Cat Token", "CAT") Ownable(msg.sender) {}
 
     // make this function visibility: external
     // pass string json uri parameter
     // restrict this function to be called only by the owner
-        // Hint: You can use OpenZeppelin Ownable contract imported above
-    
-    function mintNFT() {
+    // Hint: You can use OpenZeppelin Ownable contract imported above
+
+    function mintNFT(string memory _uri) external onlyOwner {
         // call _mint to mint a new nft to the function caller
-
+        _mint(msg.sender, _tokenIdCounter);
         // set token uri to the token id using _setTokenURI
-
+        _setTokenURI(_tokenIdCounter, _uri);
         // increment token id counter
-
+        _tokenIdCounter++;
     }
-
 }
